1b022c4e62558eddb4d5ff87b0aa79d91fee6016
erionmeh <erionm462@gmail.com>
2024-11-06 11:53:32 +0100

    Full Stack

---
+++ Lesson 25/main.js
@@ -0,0 +1,25 @@
+var x=10;
+var y=5;
+
+if(x>y){
+    console.log("This is true");
+}else{
+    console.log("This is wrong");
+}
+
+if(x<y){
+    console.log("This is wrong");
+}else{
+    console.log("This is true");
+}
+if(y>x){
+    console.log("This is true");
+}else{
+    console.log("This is wrong");
+}
+
+if(x==7){
+    console.log("10 equals 7");
+}else{
+    console.log("10 does not equal 7");
+}
---
+++ Lesson 26/main.js
@@ -0,0 +1,27 @@
+var input=document.getElementById("input_id");
+var button=document.getElementById("btn_id");
+var text=document.getElementById("text_id");
+
+button.onclick=function(){
+    text.innerHTML=input.value ;
+}
+button.onclick=function(){
+    if(input.value>10){
+        text.innerHTML="Input value is greater than 10";
+    }
+    else if(input.value<10){
+        text.innerHTML="Input value is less than 10";
+
+    }else{
+        text.innerHTML="Input value is 10";
+    }
+}
+
+var btn2=document.getElementById("answer");
+var input1=document.getElementById("input1_id");
+var input2=document.getElementById("input2_id");
+var answer=document.getElementById("text1");
+
+btn2.addEventListener("click",function(){
+answer.innerHTML=parseInt(input1.value)+parseInt(input2.value)
+})
