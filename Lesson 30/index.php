71646ceaf6ec8d3b084266c005ffa72dba70946f
ErionMeh <erionm462@gmail.com>
2024-11-25 11:26:56 +0100

    php

---
+++ Lesson 30/index.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$name = "Erion";
+echo "I am $name <br>";
+
+
+$x = 50;
+$y = 30;
+
+echo $x + $y . "<br>";
+echo $x - $y . "<br>";
+echo $x * $y . "<br>";
+echo $x / $y . "<br>";
+
+$a = "Erion";
+$b = "Mehmeti";
+
+$c = $a.$b . "<br>";
+
+echo "$c \n"; 
+
+$message = "Hello";
+$message .="World";
+
+echo $message . "<br>";
+
+$the_string = "Erion Mehmeti Test";
+
+// echo strlen($the_string);
+// echo str_word_count($the_string);
+// echo str_replace("Test", "Ma i miri", $the_string);
+echo strrev($the_string);
+
+
+
+
+
+?>
